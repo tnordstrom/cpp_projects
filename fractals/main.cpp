631d3f324cbf4a633a0f5a607a9cf3e186007e86
Tony Nordstrom <95768015+tnordstrom@users.noreply.github.com>
2024-12-11 16:09:25 -0800

    Added raylib example, doesn't build yet

--- fractals/main.cpp
+++ fractals/main.cpp
@@ -10,17 +10,50 @@
  */
 
 #include <iostream>
+#include "/cygdrive/c/raylib/raylib/src/raylib.h"
 
 double calc_iteration(double z, double c)
 {
-	double retval{ 0.0 };
-	
-	retval = (z * z) + c;
-	
-	return(retval);
+    return((z * z) + c);
 }
 
 int main(void)
 {
-	return(0);
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+ 
+    return(0);
 }
